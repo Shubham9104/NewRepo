9f15136f9c348b1bd08d53319bed108ee1ffa6ac
Shubham Mahadik <shubhammahadik91@gmail.com>
2022-07-26 15:09:50 +0530

    Add project files.

---
+++ cn_LibrarayManagement/ILib.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagement1
+{
+    public interface ILib
+    {
+        bool searchB(string s);
+        void borrow(string bor, DateTime t);
+        void returnBook(string s);
+        void Details();
+    }
+}
